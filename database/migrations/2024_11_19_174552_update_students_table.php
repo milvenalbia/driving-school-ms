be69a89ed5124b51e0adca70a1b7487406ccadc3
Milven Albia <albiav.007@gmail.com>
2024-11-20 05:02:33 +0800

    New update

---
+++ database/migrations/2024_11_19_174552_update_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropColumn(['region']);
+        
+            // Adding nullable foreign key columns
+            $table->foreignId('region')->nullable()->constrained('regions');
+            $table->foreignId('province')->nullable()->constrained('provinces');
+            $table->foreignId('municipality')->nullable()->constrained('municipalities');
+            $table->foreignId('barangay')->nullable()->constrained('barangays');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
